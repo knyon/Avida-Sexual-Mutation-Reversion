bdef27770a545232f163cbd5965cbafcc0fbafcc
Lane Smith <lane.smith@me.com>
2011-06-08 12:54:26 -0500

    Add SGE scripts from CERES

--- sge_scripts/control_cluster_script.sh
+++ sge_scripts/control_cluster_script.sh
@@ -4,7 +4,6 @@
 # SGE Options
 #$ -S /bin/bash
 #$ -N avida_sex_control
-#$ -l h_rt=72:00:00
 #$ -t 1-20
 #$ -q claus.q@compute-3-10.local,claus.q@compute-3-11.local,claus.q@compute-3-12.local,claus.q@compute-3-13.local,claus.q@compute-3-14.local,claus.q@compute-3-15.local,claus.q@compute-3-16.local,claus.q@compute-3-17.local,claus.q@compute-3-18.local,claus.q@compute-3-19.local,claus.q@compute-3-20.local,claus.q@compute-3-21.local,claus.q@compute-3-5.local,claus.q@compute-3-6.local,claus.q@compute-3-7.local,claus.q@compute-3-8.local,claus.q@compute-3-9.local
 
@@ -22,7 +21,7 @@ cd $WDIR
 
 
 # Copy Data and Config Files
-cp $HOME/avida_sex_control/* .
+cp $HOME/avida_exec/* .
 
 # Put your Science related commands here
 ./avida -s $SGE_TASK_ID >& $HOME/logs/avida_control.log
--- sge_scripts/exp_cluster_script.sh
+++ sge_scripts/exp_cluster_script.sh
@@ -3,13 +3,13 @@
 
 # SGE Options
 #$ -S /bin/bash
-#$ -N avida_sex_control
-#$ -l h_rt=72:00:00
+#$ -N avida_sex_exp
+#$ -l h_rt=36:00:00
 #$ -t 1-20
 #$ -q claus.q@compute-3-10.local,claus.q@compute-3-11.local,claus.q@compute-3-12.local,claus.q@compute-3-13.local,claus.q@compute-3-14.local,claus.q@compute-3-15.local,claus.q@compute-3-16.local,claus.q@compute-3-17.local,claus.q@compute-3-18.local,claus.q@compute-3-19.local,claus.q@compute-3-20.local,claus.q@compute-3-21.local,claus.q@compute-3-5.local,claus.q@compute-3-6.local,claus.q@compute-3-7.local,claus.q@compute-3-8.local,claus.q@compute-3-9.local
 
 WDIR=/state/partition1/$USER/$JOB_NAME-$JOB_ID-$SGE_TASK_ID
-RDIR=$HOME/avida_sex_control/results/$JOB_NAME-$JOB_ID-$SGE_TASK_ID
+RDIR=$HOME/avida_sex_exp/results/$JOB_NAME-$JOB_ID-$SGE_TASK_ID
 
 # Create Working Directory
 mkdir -p $WDIR
@@ -22,10 +22,10 @@ cd $WDIR
 
 
 # Copy Data and Config Files
-cp $HOME/avida_sex_control/* .
+cp $HOME/avida_exec/* .
 
 # Put your Science related commands here
-./avida -s $SGE_TASK_ID >& $HOME/logs/avida_control.log
+./avida -set REVERT_DETRIMENTAL 1.0 -s $SGE_TASK_ID >& $HOME/logs/avida_control.log
 
 # Copy Results Back to Home Directory
 mkdir -p $RDIR
