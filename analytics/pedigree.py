6cecedd4bb8bee98b1555f2437a5cca5f038860c
Lane Smith <lane.smith@me.com>
2011-06-30 13:33:30 -0500

    Add Breadth First Search, tested

--- analytics/pedigree.py
+++ analytics/pedigree.py
@@ -2,8 +2,25 @@
 
 import re
 import sys
+from collections import deque
 
 pedigree = {}
+
+class pedigree_queue(deque):
+
+    def __init__(self, *items):
+        super(pedigree_queue, self).__init__(self)
+        self.append(*items)
+
+    def append(self, *items):
+        for i in items:
+            super(pedigree_queue, self).append(i)
+
+def create_pedigree_from_detail_file(file_name):
+    for line in open(file_name):
+        entry = parse_detail_line(line) 
+        if entry:
+            add_genotype_to_pedigree(entry)
 
 def parse_detail_line(detail):
     regex = re.compile(r"^(\d*)[\s\)\(:a-z]*(\d*),(\d*).*heads_sex\s([a-z]*)")
@@ -18,35 +35,47 @@ def parse_detail_line(detail):
     else:
         return ''
 
-def create_pedigree_from_detail_file(file_name):
-    for line in open(file_name):
-        entry = parse_detail_line(line) 
-        if entry:
-            add_genotype_to_pedigree(entry)
-
 def add_genotype_to_pedigree(entry):
     key = entry[0]
     value = (entry[1], entry[2], entry[3])
     pedigree[key] = value
 
-def find_ancestor( 
+def pedigree_breadth_first_search(start_genotype_id, target_id):
+    bfs_queue = pedigree_queue(start_genotype_id, None)
+    node_was_visited = make_map_for_visited_nodes()
+    tree_level = 0
 
-def get_lineage_of_genotype(genotype_id, lineage=[]):
-    if genotype_id == '1':
-        lineage.append("Origin Genotype")
-    elif genotype_id not in pedigree:
-        raise Exception("No such ID: {}".format(genotype_id))
-    else:
-        parent_a_id, parent_b_id = get_parents(genotype_id)
-        get_lineage_of_genotype(parent_a_id, lineage)
-        get_lineage_of_genotype(parent_b_id, lineage)
-        lineage.append(genotype_id)
-        lineage.append(genotype_id)
-    return lineage
+    while bfs_queue:
+        print(bfs_queue)
+        node = bfs_queue.popleft()
+        if node_marks_last_in_level(node): 
+            tree_level += 1
+            bfs_queue.append(node)
+        elif node_was_visited[node] or node_was_genesis(node):
+            continue
+        elif node == target_id:
+            return (node, tree_level)
+        else:
+            bfs_queue.append(*get_parents(node))
+            node_was_visited[node] = 1
+    raise Exception("No such genotype ID")
+
+def make_map_for_visited_nodes():
+    visited_nodes_map = {}
+    for k in pedigree.keys():
+        visited_nodes_map[k] = False
+    return visited_nodes_map
+
+def node_marks_last_in_level(node):
+    return not node
 
 def get_parents(genotype_id):
     listing = pedigree[genotype_id]
     return (listing[1], listing[2])
+
+def node_was_genesis(node):
+    genesis_id = '1'
+    return node == genesis_id
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
--- analytics/test_pedigree.py
+++ analytics/test_pedigree.py
@@ -21,28 +21,33 @@ class TestParsing(unittest.TestCase):
 
 class TestPedigree(unittest.TestCase):
 
-    simple_genotypes = [(5, 'a', 4, 3),
-                        (4, 'b', 3, 2),
-                        (3, 'c', 2, 1),
-                        (2, 'd', 1, 1)]
+    simple_genotypes = [('5', 'a', '4', '3'),
+                        ('4', 'b', '3', '2'),
+                        ('3', 'c', '2', '1'),
+                        ('2', 'd', '1', '1')]
     for node in simple_genotypes:
         pedigree.add_genotype_to_pedigree(node)
 
     def test_simple_pedigree_construction(self):
-        expected_pedigree = { 5: ('a', 4, 3),
-                            4: ('b', 3, 2),
-                            3: ('c', 2, 1),
-                            2: ('d', 1, 1) }
+        expected_pedigree = { '5': ('a', '4', '3'),
+                            '4': ('b', '3', '2'),
+                            '3': ('c', '2', '1'),
+                            '2': ('d', '1', '1') }
         self.assertDictEqual(expected_pedigree, pedigree.pedigree)
 
     def test_get_parents_returns_correct_parents(self):
-        self.assertTupleEqual(pedigree.get_parents(5), (4, 3))
-        self.assertTupleEqual(pedigree.get_parents(2), (1, 1))
+        self.assertTupleEqual(pedigree.get_parents('5'), ('4', '3'))
+        self.assertTupleEqual(pedigree.get_parents('2'), ('1', '1'))
 
-    def test_ID_not_in_pedigree_raises_exception(self):
+    def test_search_for_ID_not_in_pedigree_raises_exception(self):
         number_not_in_pedigree = 10
-        self.assertRaises(Exception, pedigree.get_lineage_of_genotype,
+        self.assertRaises(Exception, pedigree.pedigree_breadth_first_search,
                 number_not_in_pedigree)
+
+    def test_bfs(self):
+        result = pedigree.pedigree_breadth_first_search('5', '2')
+        self.assertTupleEqual(result, ('2', 2))
+
 
 
 if __name__ == '__main__':
