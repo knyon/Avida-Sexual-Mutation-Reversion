f023aa22b85c4daf8c071a081f0bb6a23e37f62b
Lane Smith <lane.smith@me.com>
2011-11-15 23:07:05 -0600

    Fixed bug in evaluator. Need to push to test on enlil

--- analytics/Avida_Pedigree/pedigree/evaluator.py
+++ analytics/Avida_Pedigree/pedigree/evaluator.py
@@ -4,15 +4,19 @@ import re
 
 class MutationEvaluator:
 
-    def __init__(self, pathToAvida = (os.path.curdir + os.path.sep + "avida")):
+    def __init__(self, pathToAvida = (os.path.curdir + os.path.sep)):
         self.pathToAvida = pathToAvida
 
     def evaluate_effect_of_mutation(self, genotype, mutation):
         genotypeFitness = self.get_fitness_of_sequence(genotype.sequence)
         revertedSequence = genotype.get_sequence_with_mutation_reverted(mutation)
         revertedFitness = self.get_fitness_of_sequence(revertedSequence)
-        if revertedFitness > genotypeFitness:
+        print(genotypeFitness)
+        print(revertedFitness)
+        if genotypeFitness > revertedFitness:
             return 1
+        elif genotypeFitness == revertedFitness:
+            return 0
         else:
             return -1
 
@@ -24,21 +28,21 @@ class MutationEvaluator:
     def write_sequence_to_avida_analyze_file(self, sequence):
         analyzeFilePath = self.pathToAvida + "analyze.cfg"
         output = 'LOAD_SEQUENCE {0}\nRECALCULATE\nDETAIL detail.dat fitness'.format(sequence)
-        if(os.path.exists(analyzeFilePath)):
-            os.remove(analyzeFilePath)
+        #if(os.path.exists(analyzeFilePath)):
+            #os.remove(analyzeFilePath)
         fp = open(analyzeFilePath, 'w')
         fp.write(output)
         fp.close()
 
     def run_avida_in_analyze_mode(self):
-        command = self.pathToAvida + "avida -a"
+        command = self.pathToAvida + "avida -a > /dev/null 2> /dev/null"
         os.system(command)
 
     def get_fitness_from_analyze_output_file(self):
-        outputFilePath = self.pathToAvida + os.path.sep + 'detail.txt'
+        outputFilePath = self.pathToAvida + "data" + os.path.sep + 'detail.dat'
         fp = open(outputFilePath)
         analyzeOutputFile = fp.read()
         fp.close()
-        fitnessRegex = re.search(analyzeOutputFile, "^\d+.?\d*")
+        fitnessRegex = re.search("^\d+(\.\d*)?", analyzeOutputFile, re.MULTILINE)
         return float(fitnessRegex.group(0))
         
