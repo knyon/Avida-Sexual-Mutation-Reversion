edc635446c05762864333b3993b27f670575dc88
Lane Smith <lane.smith@me.com>
2011-11-21 03:26:41 -0600

    Trying to find that stupid key error bug

--- analytics/Avida_Pedigree/pedigree/genotype.py
+++ analytics/Avida_Pedigree/pedigree/genotype.py
@@ -49,8 +49,12 @@ class Genotype():
         return mutationCount
     
     def sequence_contains_mutation(self, subMut):
-        if subMut[1] > len(self.sequence): return False
-        return self.sequence[subMut[1]] == subMut[2]
+        try:
+            if subMut[1] > len(self.sequence): return False
+            return self.sequence[subMut[1]] == subMut[2]
+        except:
+            print("Key error sub mut: {}".format(subMut))
+
 
     def get_sequence_with_mutation_reverted(self, subMut):
         revertedSequence = list(self.sequence)
