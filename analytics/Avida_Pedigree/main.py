f4440c7759dd6c59997af5f870c8e66e0f790abf
Lane Smith <lane.smith@me.com>
2011-11-27 17:17:22 -0600

    All use of Tracer class removed.
    Will use get-things-done approach from now on. *Not* DRY at all, but
    feh.

--- analytics/Avida_Pedigree/main.py
+++ analytics/Avida_Pedigree/main.py
@@ -13,41 +13,44 @@ output = open("analysis.txt", 'w')
 output.write("Analysis file:")
 
 def analyze_lineage(genealogy, dominantLineage):
-    edgeTotal = len(dominantLineage)
-    edgeCount = 0
     queue = deque()
     queue.append(genealogy['1'])
     while queue:
         parent = queue.popleft()
         for offspring in [genealogy[ID] for ID in parents.children]:
-            if parent.fitness > offspring.fitness and offspring.num_sub_mutations() > 0:
-                for mutation in [m for m in offspring.mutations if m]:
-                    if evaluator.evaluate_effect_of_mutation(offspring, mutation) < 0:
-                        analyze_deleterious_mutation(genealogy, offspring, mutation)
-            queue.append(offspring)
+            if not offspring.isMarked():
+                if parent.fitness > offspring.fitness and offspring.num_sub_mutations() > 0:
+                    for mutation in [m for m in offspring.mutations if m]:
+                        if evaluator.evaluate_effect_of_mutation(offspring, mutation) < 0:
+                            analyze_deleterious_mutation(genealogy, offspring, mutation)
+                queue.append(offspring)
 
 def analyze_deleterious_mutation(genealogy, origin, mutation):
-    pattern = SubMutTDTracePattern(mutation)
-    mutationTrace = Tracer(genealogy, pattern).make_trace(origin)
-    if mutationTrace:
-        for parentID, offspringID in mutationTrace:
-            parent = genealogy[parentID]
-            offspring = genealogy[offspringID]
-            if offspring.fitness > parent.fitness and offspring.num_sub_mutations() > 0 and evaluator.evaluate_effect_of_mutation(offspring, mutation) > 0:
-                output.write("\nSign epistatic occurance found:\n")
-                output.write("\tOrigin ID  : {}\n".format(origin.ID))
-                output.write("\tParent ID  : {}\n".format(parent.ID))
-                output.write("\tRecovery ID: {}\n".format(offspring.ID))
-                output.write("\tOrigin sequence  : {}\n".format(origin.sequence))
-                output.write("\tParent sequence  : {}\n".format(parent.sequence))
-                output.write("\tRecovery sequence: {}\n".format(offspring.sequence))
-                output.write("\tDeleterious mutation: {0} to {2} at {1}\n".format(*mutation))
-                for mutation in [m for m in offspring.mutations if m]:
-                    output.write("\tRecovery mutation  : {0} to {2} at {1}\n".format(*mutation))
-                output.write("\tOrigin fitness   : {}\n".format(origin.fitness))
-                output.write("\tParent fitness   : {}\n".format(parent.fitness))
-                output.write("\tRecovery fitness: {}\n".format(offspring.fitness))
-                return
+    queue = deque()
+    queue.append(origin)
+    while queue:
+        parent = queue.popleft()
+        for offspring in [genealogy[ID] for ID in parents.children]:
+            if offspring.sequence_contains_mutation(mutation):
+                if offspring.fitness > parent.fitness and offspring.num_sub_mutations() > 0 and evaluator.evaluate_effect_of_mutation(offspring, mutation) > 0:
+                    output.write("\nSign epistatic occurance found:\n")
+                    output.write("\tOrigin ID  : {}\n".format(origin.ID))
+                    output.write("\tParent ID  : {}\n".format(parent.ID))
+                    output.write("\tRecovery ID: {}\n".format(offspring.ID))
+                    output.write("\tOrigin sequence  : {}\n".format(origin.sequence))
+                    output.write("\tParent sequence  : {}\n".format(parent.sequence))
+                    output.write("\tRecovery sequence: {}\n".format(offspring.sequence))
+                    output.write("\tDeleterious mutation: {0} to {2} at {1}\n".format(*mutation))
+                    for mutation in [m for m in offspring.mutations if m]:
+                        output.write("\tRecovery mutation  : {0} to {2} at {1}\n".format(*mutation))
+                    output.write("\tOrigin fitness   : {}\n".format(origin.fitness))
+                    output.write("\tParent fitness   : {}\n".format(parent.fitness))
+                    output.write("\tRecovery fitness: {}\n".format(offspring.fitness))
+                    return None
+                else: 
+                    queue.append(offspring)
+
+
 
 
 if __name__ == '__main__':
