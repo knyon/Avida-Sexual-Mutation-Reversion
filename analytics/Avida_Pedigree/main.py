a8466ff06a363131d97293b50d475536e7eeeed8
Lane Smith <lane.smith@me.com>
2011-11-29 15:08:58 -0600

    Add branch-n-bound and Mark Maps

--- analytics/Avida_Pedigree/main.py
+++ analytics/Avida_Pedigree/main.py
@@ -8,6 +8,28 @@ from pedigree.tracer import *
 from pedigree.evaluator import *
 from collections import deque
 
+class SearchMarker:
+
+    def __init__(self):
+        self.markerMap = {}
+
+    def mark(self, genotypeID):
+        self.markerMap[genotypeID] = True
+
+    def unmark(self, genotypeID )
+        self.markerMap[genotypeID] = False
+
+    def unmark_all(self):
+        del self.MarkerMap
+        self.markerMap = {}
+
+    def is_marked(self, genotypeID):
+        if genotypeID in self.markerMap:
+            return self.markerMap[genotypeID]
+        else:
+            self.markerMap[genotypeID] = False
+            return False
+
 evaluator = MutationEvaluator()
 output = open("analysis.txt", 'w')
 output.write("Analysis file:")
@@ -25,34 +47,38 @@ def analyze_lineage(genealogy):
                 if parent.fitness > offspring.fitness and offspring.num_sub_mutations() > 0:
                     for mutation in [m for m in offspring.mutations if m]:
                         if evaluator.evaluate_effect_of_mutation(offspring, mutation) < 0:
-                            analyze_deleterious_mutation(genealogy, offspring, mutation)
+                            if not analyze_deleterious_mutation(genealogy, offspring, mutation)
+                                queue.append(offspring)
                 offspring.mark()
-                queue.append(offspring)
 
 def analyze_deleterious_mutation(genealogy, origin, mutation):
+    marker = SearchMarker()
     queue = deque()
     queue.append(origin)
     while queue:
         parent = queue.popleft()
-        for offspring in [genealogy[ID] for ID in parent.children]:
-            if offspring.sequence_contains_mutation(mutation):
-                if offspring.fitness > parent.fitness and offspring.num_sub_mutations() > 0 and evaluator.evaluate_effect_of_mutation(offspring, mutation) > 0:
-                    output.write("\nSign epistatic occurance found:\n")
-                    output.write("\tOrigin ID  : {}\n".format(origin.ID))
-                    output.write("\tParent ID  : {}\n".format(parent.ID))
-                    output.write("\tRecovery ID: {}\n".format(offspring.ID))
-                    output.write("\tOrigin sequence  : {}\n".format(origin.sequence))
-                    output.write("\tParent sequence  : {}\n".format(parent.sequence))
-                    output.write("\tRecovery sequence: {}\n".format(offspring.sequence))
-                    output.write("\tDeleterious mutation: {0} to {2} at {1}\n".format(*mutation))
-                    for mutation in [m for m in offspring.mutations if m]:
-                        output.write("\tRecovery mutation  : {0} to {2} at {1}\n".format(*mutation))
-                    output.write("\tOrigin fitness   : {}\n".format(origin.fitness))
-                    output.write("\tParent fitness   : {}\n".format(parent.fitness))
-                    output.write("\tRecovery fitness: {}\n".format(offspring.fitness))
-                    return None
-                else: 
-                    queue.append(offspring)
+        if not marker.is_marked(parent.ID):
+            for offspring in [genealogy[ID] for ID in parent.children]:
+                if offspring.sequence_contains_mutation(mutation):
+                    if offspring.fitness > parent.fitness and offspring.num_sub_mutations() > 0 and evaluator.evaluate_effect_of_mutation(offspring, mutation) > 0:
+                        output.write("\nSign epistatic occurance found:\n")
+                        output.write("\tOrigin ID  : {}\n".format(origin.ID))
+                        output.write("\tParent ID  : {}\n".format(parent.ID))
+                        output.write("\tRecovery ID: {}\n".format(offspring.ID))
+                        output.write("\tOrigin sequence  : {}\n".format(origin.sequence))
+                        output.write("\tParent sequence  : {}\n".format(parent.sequence))
+                        output.write("\tRecovery sequence: {}\n".format(offspring.sequence))
+                        output.write("\tDeleterious mutation: {0} to {2} at {1}\n".format(*mutation))
+                        for mutation in [m for m in offspring.mutations if m]:
+                            output.write("\tRecovery mutation  : {0} to {2} at {1}\n".format(*mutation))
+                        output.write("\tOrigin fitness   : {}\n".format(origin.fitness))
+                        output.write("\tParent fitness   : {}\n".format(parent.fitness))
+                        output.write("\tRecovery fitness: {}\n".format(offspring.fitness))
+                        del marker
+                        return True
+                    else: 
+                        queue.append(offspring)
+    return False
 
 
 if __name__ == '__main__':
@@ -64,9 +90,8 @@ if __name__ == '__main__':
     genealogy = GenealogyMaker().make_genealogy_from_file(fileName, dominantGenotypeID)
     genealogy.prune_all_non_lineage_genotypes()
     print "Genologized!"
-    # No need to trace, because the pruned genealogy is the trace
-    #dominantLineage = Tracer(genealogy, TopDownTracePattern()).make_trace(genesisGenotype)
-    #print "Traced!"
     analyze_lineage(genealogy)
     print("Finished!")
     output.close()
+
+
