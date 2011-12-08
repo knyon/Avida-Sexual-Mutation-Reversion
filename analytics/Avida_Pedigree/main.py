df4871282032eafab6468e63e15bb9b57658d884
Lane Smith <lane.smith@me.com>
2011-12-08 01:21:50 -0600

    Add CSV output, more values output

--- analytics/Avida_Pedigree/main.py
+++ analytics/Avida_Pedigree/main.py
@@ -31,10 +31,31 @@ class SearchMarker:
             return False
 
 evaluator = MutationEvaluator()
-detailedOutput = open("analysis.txt", 'w')
+analysisOutput = open("analysis.txt", 'w')
+analysisOutputHeader = '''# Analysis of all sign epsistatic events that occurred in the lineage of the final dominant genotype
+# Values separated by commas, in order:
+# 1. Deleterious mutation
+# 2. Genotype ID of origin of deleterious mutation
+# 3. Origin fitness
+# 4. Origin sequence
+# 5. Parent 1 of origin ID
+# 6. Parent 1 of origin fitness
+# 7. Parent 1 of origin sequence
+# 8. Parent 2 of origin ID
+# 9. Parent 2 of origin fitness
+# 10. Parent 2 of origin sequence
+# 11. Genotype ID of recovery genotype
+# 12. Recovery fitness
+# 13. Recovery sequence
+# 14. All mutations in recovery genotype
+# 15. Parent 1 of recovery ID
+# 16. Parent 1 of recovery fitness
+# 17. Parent 1 of recovery sequence
+# 18. Parent 2 of recovery ID
+# 19. Parent 2 of recovery fitness
+# 20. Parent 2 of recovery sequence\n\n\n'''
+analysisOutput.write(analysisOutputHeader)
 summaryOutput = open("summary.txt", 'w')
-detailedOutput.write("Analysis file:")
-averageFitnessIncreaseBin = 0
 
 def analyze_lineage(genealogy):
     statusCount = 0
@@ -49,7 +70,7 @@ def analyze_lineage(genealogy):
             if not offspring.isMarked():
                 if parent.fitness > offspring.fitness and offspring.num_sub_mutations() > 0 and parent.fitness - offspring.fitness > parent.fitness*0.01:
                     for mutation in [m for m in offspring.mutations if m]:
-                        if evaluator.evaluate_effect_of_mutation(offspring, mutation) < 0:
+                        if mutation[0] != mutation[2] and evaluator.evaluate_effect_of_mutation(offspring, mutation) < 0:
                             delMutationCount += 1
                             if analyze_deleterious_mutation(genealogy, offspring, mutation):
                                 signEpistasisCount += 1
@@ -62,8 +83,6 @@ def analyze_lineage(genealogy):
         if statusCount % 100 == 0:                                                                                                                                                
             print("On item {} through the queue".format(statusCount))
     summaryOutput.write("Total Deleterious Mutations: {}\nTotal Sign Epistatic Mutations: {}".format(delMutationCount, signEpistasisCount))
-    if signEpistasisCount > 0:
-        summaryOutput.write("\nAverage increase in fitness with SE event: {}".format(averageFitnessIncreaseBin/signEpistasisCount))
 
 def analyze_deleterious_mutation(genealogy, origin, delMutation):
     marker = SearchMarker()
@@ -75,20 +94,20 @@ def analyze_deleterious_mutation(genealogy, origin, delMutation):
             for offspring in [genealogy[ID] for ID in parent.children]:
                 if offspring.sequence_contains_mutation(delMutation):
                     if offspring.fitness > parent.fitness and offspring.num_sub_mutations() > 0 and offspring.fitness - parent.fitness > parent.fitness*0.01 and evaluator.evaluate_effect_of_mutation(offspring, delMutation) > 0:
-                        detailedOutput.write("\nSign epistatic occurance found:\n")
-                        detailedOutput.write("\tOrigin ID  : {}\n".format(origin.ID))
-                        detailedOutput.write("\tParent ID  : {}\n".format(parent.ID))
-                        detailedOutput.write("\tRecovery ID: {}\n".format(offspring.ID))
-                        detailedOutput.write("\tOrigin sequence  : {}\n".format(origin.sequence))
-                        detailedOutput.write("\tParent sequence  : {}\n".format(parent.sequence))
-                        detailedOutput.write("\tRecovery sequence: {}\n".format(offspring.sequence))
-                        detailedOutput.write("\tDeleterious mutation: {0} to {2} at {1}\n".format(*delMutation))
-                        for mutation in [m for m in offspring.mutations if m]:
-                            detailedOutput.write("\tRecovery mutation  : {0} to {2} at {1}\n".format(*mutation))
-                            check_if_SE_mutations_in_final_dominant(delMutation, mutation, offspring.ID, genealogy)
-                        detailedOutput.write("\tOrigin fitness   : {}\n".format(origin.fitness))
-                        detailedOutput.write("\tParent fitness   : {}\n".format(parent.fitness))
-                        detailedOutput.write("\tRecovery fitness: {}\n".format(offspring.fitness))
+                        analysisLine = '''{deleteriousMutation},{originID},{originFitness},{originSequence},\
+                        {originParent1ID},{originParent1Fitness},{originParent1Sequence},\
+                        {originParent2ID},{originParent2Fitness},{originParent2Sequence},\
+                        {recoveryID},{recoveryFitness},{recoverySequence},{recoveryMutations},\
+                        {recoveryParent1ID},{recoveryParent1Fitness},{recoveryParent1Sequence},\
+                        {recoveryParent2ID}, {recoveryParent2Fitness},{recoveryParent2Sequence}'''\
+                        .format(deleteriousMutation=delMutation, originID=origin.ID, originFitness=origin.fitness, originSequence=origin.sequence,
+                                originParent1ID=genealogy[origin.parents[0]].ID, originParent1Fitness=genealogy[origin.parents[0]].fitness, originParent1Sequence=genealogy[origin.parents[0]].sequence,
+                                originParent2ID=genealogy[origin.parents[1]].ID, originParent2Fitness=genealogy[origin.parents[1]].fitness, originParent2Sequence=genealogy[origin.parents[1]].sequence,
+                                recoveryID=offspring.ID, recoveryFitness=offspring.fitness, recoverySequence=offspring.sequence, recoveryMutations=offspring.mutations,
+                                recoveryParent1ID=genealogy[offspring.parents[0]].ID, recoveryParent1Fitness=genealogy[offspring.parents[0]].fitness, recoveryParent1Sequence=genealogy[offspring.parents[0]].sequence,
+                                recoveryParent2ID=genealogy[offspring.parents[1]].ID, recoveryParent2Fitness=genealogy[offspring.parents[1]].fitness, recoveryParent2Sequence=genealogy[offspring.parents[1]].sequence
+                                )
+                        analysisOutput.write(analysisLine)
                         del marker
                         return offspring.fitness - parent.fitness
                     else: 
@@ -114,5 +133,5 @@ if __name__ == '__main__':
     print "Genologized!"
     analyze_lineage(genealogy)
     print("Finished!")
-    detailedOutput.close()
+    analysisOutput.close()
     summaryOutput.close()
